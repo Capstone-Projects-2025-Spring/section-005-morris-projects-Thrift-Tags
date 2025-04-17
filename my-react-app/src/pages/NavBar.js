313c7dcf89a62443548ad98584c10ec62e4132c4
Jie Huang <huanyugedizifeng@gmail.com>
2025-04-17 13:22:43 -0400

    Save

--- my-react-app/src/pages/NavBar.js
+++ my-react-app/src/pages/NavBar.js
@@ -23,7 +23,7 @@ export default function NavBar() {
             </div>
             <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
                 <CustomLink to="/events">Events</CustomLink>
-                <CustomLink to="/friends">Friends List</CustomLink>
+                <CustomLink to="/friends">Friends</CustomLink>
                 <CustomLink to="/profile">Profile</CustomLink>
             </ul>
         </nav>
