c0ef5908dc460829bb10582972d8a63dcc318b6b
Jie Huang <huanyugedizifeng@gmail.com>
2025-04-22 13:09:44 -0400

    fixed error

--- my-react-app/src/pages/NavBar.js
+++ my-react-app/src/pages/NavBar.js
@@ -23,9 +23,9 @@ export default function NavBar() {
             </div>
             <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
                 <CustomLink to="/events">Events</CustomLink>
-                <CustomLink to="/friends">Friends List</CustomLink>
+                <CustomLink to="/friends">Friends</CustomLink>
                 <CustomLink to="/add-review">Add Review</CustomLink>
-                <CustomLink to="/reviews">My Reviews</CustomLink>
+                <CustomLink to="/reviews">Reviews</CustomLink>
                 <CustomLink to="/profile">Profile</CustomLink>
             </ul>
         </nav>
