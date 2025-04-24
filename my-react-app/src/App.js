64304c40232c5061f70e1236d5f58d1c00ca6fa6
BNguyen0617 <156949338+BNguyen0617@users.noreply.github.com>
2025-04-24 12:59:06 -0400

    THT-48: added a functionality to view your friend's friends list and be able to add them to your friends

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -10,6 +10,7 @@ import ReviewsPage from './pages/ReviewsPage';
 import FriendPage from './pages/FriendPage';
 import AddReviewPage from './pages/AddReviewPage';
 import FriendProfilePage from './pages/FriendProfilePage';
+import FriendFriendsPage from './pages/FriendFriendsPage';
 
 
 function App() {
@@ -32,6 +33,7 @@ function App() {
                 <Route path="/friends" element={<FriendPage/>}/>
                 <Route path="/add-review" element={<AddReviewPage/>}/>
                 <Route path="/friendprofile" element={<FriendProfilePage/>}/>
+                <Route path="/friend-friends" element={<FriendFriendsPage/>}/>
             </Routes>
         </Router>
     );
